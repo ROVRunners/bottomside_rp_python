3b5664059da6c531735cc36521518e21e6c06dc0
Jason Fanger <jasonfanger6695@gmail.com>
2024-11-17 19:22:56 -0800

    Beginnings of the bottomside code

---
+++ bottomside/__main__.py
@@ -0,0 +1,19 @@
+import bottomside.spike as spike
+
+if __name__ == '__main__':
+    # Set to True to close the program when Spike exits without an error through modifying Spike.run.
+    intentionally_shut_down = False
+
+    while not intentionally_shut_down:
+        try:
+            # Create an instance of the Spike class.
+            main_system = spike.Spike()
+
+            # Loop until Spike.run is set to False, then close the program.
+            while main_system.run:
+                main_system.loop()
+
+            intentionally_shut_down = True
+
+        except Exception as e:
+            print(f'Exception in main_system.main_loop():\n{e}')
---
+++ bottomside/spike.py
@@ -0,0 +1,11 @@
+import bottomside.IMU as IMU
+import bottomside.motors as motors
+import bottomside.surface_connection as connection
+
+
+class Spike:
+    def __init__(self):
+        self.run = True
+
+    def loop(self):
+        pass
