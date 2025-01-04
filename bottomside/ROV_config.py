a906764593066b32eaf380a00c0d18384413df0f
Jason Fanger <jasonfanger6695@gmail.com>
2025-01-04 15:50:45 -0800

    changed ip

--- bottomside/ROV_config.py
+++ bottomside/ROV_config.py
@@ -4,6 +4,6 @@ from dataclasses import dataclass
 @dataclass
 class ROV:
     rov_name: str = "ROV"
-    ip: str = ""
+    ip: str = "192.168.2.3"
     video_port: int = 5600
     comms_port: int = 1883
