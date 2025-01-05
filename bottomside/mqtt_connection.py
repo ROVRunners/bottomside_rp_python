fbf779659912cd1edc207cdb33e92be0d4cd0156
Jason Fanger <jasonfanger6695@gmail.com>
2025-01-04 17:38:23 -0800

    Test5

--- bottomside/mqtt_connection.py
+++ bottomside/mqtt_connection.py
@@ -141,8 +141,6 @@ class MQTTConnection:
             self._client.publish(topic, payload)
 
     def _on_message(self, client, userdata, message):
-        print(f"Received message '{message.payload.decode()}' on topic '{message.topic}'")
-
         if message.topic == "PC/commands/subscribe":
             self.subscribe(message.payload.decode())
         else:
