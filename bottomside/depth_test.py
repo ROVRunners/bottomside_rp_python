026ecb94e030ccb252d87af09deab4fc215baecf
Jason Fanger <jasonfanger6695@gmail.com>
2025-02-21 14:14:02 -0800

    Testing other depth sensor

--- bottomside/depth_test.py
+++ bottomside/depth_test.py
@@ -1,21 +1,25 @@
 import ms5837
 import time
 
-sensor = ms5837.MS5837_02BA() # Default I2C bus is 1 (Raspberry Pi 3)
+sensor = ms5837.MS5837_30BA()  # Default I2C bus is 1 (Raspberry Pi 3)
 
 # We must initialize the sensor before reading it
 if not sensor.init():
-        print("Sensor could not be initialized")
-        exit(1)
+    print("Sensor could not be initialized")
+    exit(1)
 
 # Print readings
 while True:
-        if sensor.read():
-                print(("P: %0.1f mbar  %0.3f psi\tT: %0.2f C  %0.2f F") % (
-                sensor.pressure(), # Default is mbar (no arguments)
-                sensor.pressure(ms5837.UNITS_psi), # Request psi
-                sensor.temperature(), # Default is degrees C (no arguments)
-                sensor.temperature(ms5837.UNITS_Farenheit))) # Request Farenheit
-        else:
-                print("Sensor read failed!")
-                exit(1)
+    if sensor.read():
+        print("P: %0.1f mbar  %0.3f psi\tT: %0.2f C  %0.2f F\tD: %0.2f m  %0.2f m" % (
+            sensor.pressure(),  # Default is mbar (no arguments)
+            sensor.pressure(ms5837.UNITS_psi),  # Request psi
+            sensor.temperature(),  # Default is degrees C (no arguments)
+            sensor.temperature(ms5837.UNITS_Farenheit),  # Request Farenheit
+            sensor.depth(),  # Default is meters (no arguments)
+            sensor.altitude()  # Default is meters (no arguments)
+              ))
+
+    else:
+        print("Sensor read failed!")
+        exit(1)
