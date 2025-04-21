67776be4263c03874752cdec1c7e2db5d53f4fc3
Jason Fanger <jasonfanger6695@gmail.com>
2025-04-21 16:58:24 -0700

    Exception handling and more debugging

--- bottomside/custom_sensors/depth_sensor.py
+++ bottomside/custom_sensors/depth_sensor.py
@@ -13,7 +13,7 @@ class DepthSensor(GenericSensor):
         if not self.sensor.init():
             print("Sensor could not be initialized")
         else:
-            print("Sensor initialized successfully")
+            print(f"Sensor {self.sensor} initialized successfully")
 
     def get_data(self) -> dict[str, float]:
         """Get the depth, pressure, and temperature from the sensor.
@@ -22,14 +22,22 @@ class DepthSensor(GenericSensor):
             dict[str, float]: The depth, pressure, and temperature.
         """
         # Print readings
-        if self.sensor.read():
-            return {
-                "depth": self.sensor.depth(),
-                "pressure_mbar": self.sensor.pressure(),  # Default is mbar (no arguments)
-                "temperature_C": self.sensor.temperature(),  # Default is degrees C (no arguments)
-            }
-        else:
-            print("Sensor read failed!")
+        try:
+            if self.sensor.read():
+                return {
+                    "depth": self.sensor.depth(),
+                    "pressure_mbar": self.sensor.pressure(),  # Default is mbar (no arguments)
+                    "temperature_C": self.sensor.temperature(),  # Default is degrees C (no arguments)
+                }
+            else:
+                print("Sensor read failed!")
+                return {
+                    "depth": 0,
+                    "pressure_mbar": 0,
+                    "temperature_C": 0,
+                }
+        except Exception as e:
+            print(f"Error reading sensor: {e}")
             return {
                 "depth": 0,
                 "pressure_mbar": 0,
