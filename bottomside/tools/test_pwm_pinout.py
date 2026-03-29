60e8b7bebde50a0fa3ce4630b3d87d99e6b880f7
Jason Fanger <jasonfanger6695@gmail.com>
2026-03-28 19:30:25 -0700

    Added pwm pinout test

---
+++ bottomside/tools/test_pwm_pinout.py
@@ -0,0 +1,23 @@
+input("WARNING! Only continue if the ESCs are disconnected!")
+
+import pigpio
+
+gpio: pigpio.pi = pigpio.pi()
+
+pin: int = int(input("Gimme pin number: "))
+new_pin: int = pin
+
+gpio.set_mode(pin, pigpio.OUTPUT)
+gpio.write(pin)
+
+while True:
+    new_pin = int(input("Gimme pin number: "))
+
+    # Shut down the old pin.
+    gpio.set_mode(pin, pigpio.OUTPUT)
+    gpio.write(pin, 0)
+
+    # Start up the new pin.
+    pin = new_pin
+    gpio.set_mode(pin, pigpio.OUTPUT)
+    gpio.write(pin, 1)
